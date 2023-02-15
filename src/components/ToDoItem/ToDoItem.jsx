56df965cdc3d5005819b4abf1fdc53bcab65a8f9
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-15 12:47:31 +0000

    items adding to list when input

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Form from '../Form/Form'
+import './ToDoItem.scss'
 
 const ToDoItem = ({todos, completedTodo}) => {
   const [edit, setEdit] =useState({
@@ -9,13 +10,13 @@ const ToDoItem = ({todos, completedTodo}) => {
   })
 
   return todos.map((todo, index) => (
-    <div className='' key={index}>
+    <div className='todo__item' key={index}>
       <div 
         key={todo.id} 
         onClick={()=>completedTodo(todo.id)}>
         {todo.text}
       </div>
-
+      <button className='todo__item--button'>x</button>
     </div>
   ))
 }
